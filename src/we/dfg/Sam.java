62d7bac80bcb9a2c087cf3c77bfd6a7c654254ee
ArunishSVA <arunishshiva.apr12@gmail.com>
2023-02-12 00:30:49 +0530

    Code added newly

--- src/we/dfg/Sam.java
+++ src/we/dfg/Sam.java
@@ -5,5 +5,7 @@ public static void main(String[] args) {
 	System.out.println("First ");
 	System.out.println("you can");
 	System.out.println("All  the Best");
+	System.out.println("My father name is vivekananthan");
+	System.out.println("My mother name is shakila");
 }
 }
